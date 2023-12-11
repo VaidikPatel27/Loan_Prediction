a512e8bc8821bf5e3c63e65e691bf9da767f6923
Vaidik Patel <vaidikpatel207@gmail.com>
2023-12-11 02:28:00 +0000

    port change

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ def predict_sales():
     return render_template('index.html',result=outcome)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host = '0.0.0.0' , port = 8000 , debug=True)
